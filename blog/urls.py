6380c9ee471b26e2e914bcae3d6f704d5ee36697
mgsssss <mgsssss@naver.com>
2023-11-17 15:53:31 +0900

    [feat] blog templates basic and list view

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+
+    path("list/", views.blog_list),
+    path("list2/", views.BlogListView.as_view()),
+    path("<int:pk>/", views.BlogDetailView.as_view()),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
+import random
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from .models import Blog
 
-# Create your views here.
+def blog_list(request, *args, **kwargs):
+    context = {}
+    context['number'] = random.randint(1 ,1000)
+    if request.method == 'GET':
+        context['blogs'] = Blog.objects.all()
+
+    return render(request, 'blog/blog_list.html', context)
+
+
+class BlogListView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['number'] = random.randint(1 ,1000)
+        context['blogs'] = Blog.objects.all()
+        return render(request, 'blog/blog_list.html', context)
+    
+class BlogDetailView(View):
+    def get(self, request, pk):
+        
+        context = {}
+        context['blogs'] = [Blog.objects.get(id=pk)]
+        return render(request, 'blog/blog_list.html', context)     
