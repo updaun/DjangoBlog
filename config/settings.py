80cc9a59f652178fe7d18c11b69f2eb4f8c23688
updaun <updauney@daum.net>
2023-11-23 19:27:49 +0900

    [feat] custom pagination

--- config/settings.py
+++ config/settings.py
@@ -143,5 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
+    'PAGE_SIZE': 10
 }
