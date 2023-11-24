004e6ed09cecf6ac653531e0f0f0bb9ecf2a251e
updaun <updauney@daum.net>
2023-11-24 14:19:47 +0900

    [deploy] settings separate, requirements.txt update

---
+++ config/settings/deploy.py
@@ -0,0 +1,25 @@
+from .base import *
+
+def read_secret(secret_name):
+    file = open("/run/secrets/" + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': read_secret("POSTGRES_DB"),
+        'USER': read_secret("POSTGRES_USER"),
+        'PASSWORD': read_secret("POSTGRES_PASSWORD"),
+        'HOST': 'postgresdb',
+        'PORT': '5432',
+    }
+}
---
+++ config/settings/local.py
@@ -0,0 +1,23 @@
+from .base import *
+
+import os, environ
+from pathlib import Path
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+DEBUG = env("DEBUG")
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
