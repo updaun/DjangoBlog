37f706ba5c0ff5e27d4c1ff397c1ebebf6e8dc3c
mgsssss <mgsssss@naver.com>
2023-11-27 20:53:02 +0900

    temp2

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -1,6 +1,5 @@
 from .base import *
 
-
 def read_secret(secret_name):
     file = open("/run/secrets/" + secret_name)
     secret = file.read()
@@ -8,7 +7,8 @@ def read_secret(secret_name):
     file.close()
     return secret
 
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
+
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
